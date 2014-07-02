813b520ab6db400678936f3d89e9e715405fcd67
rootadin <dennis.biil@gmail.com>
2014-07-02 23:39:51 +0200

    Initial mod maker, MDI parent setup.

---
+++ Storwins-Mod.Core/Models/ModOptional.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Storwins_Mod.Core.Models
+{
+    [Serializable]
+    [XmlRoot("storwins_mod")]
+    public class ModOptional
+    {
+        [XmlElement("name")]
+        public String Name { get; set; }
+
+        [XmlElement("types")]
+        public String Types { get; set; }
+
+        [XmlElement("is_installed")]
+        public Boolean IsInstalled { get; set; }
+
+        [XmlArray("files")]
+        [XmlArrayItem("filename")]
+        public List<String> Files { get; set; }
+    }
+}
