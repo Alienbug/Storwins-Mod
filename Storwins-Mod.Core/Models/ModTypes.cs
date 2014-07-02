a70a67a89f2c3fda6fa450dac44b5dbf683dd318
rootadin <dennis.biil@gmail.com>
2014-07-03 00:13:11 +0200

    Missing files.

---
+++ Storwins-Mod.Core/Models/ModTypes.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Storwins_Mod.Core.Models
+{
+    public enum ModTypes
+    {
+        Crosshair,
+        DamageIndicator,
+        DamagePanel,
+        Garage,
+        GarageClock,
+        GunConstaints,
+        Icons,
+        HitZoneSkins,
+        Misc,
+        Minimap,
+        OverTargetMarkers,
+        ServersideCrosshair,
+        SessionStatistics,
+        Sound,
+        Skins,
+        Techtree,
+        Zoom,
+        Xvm,
+        XvmConfig,
+        XvmSixtSenseIcon,
+        XvmSixtSenseSound
+    }
+
+    public class CanInstallMultiple
+    {
+        public List<ModTypes> Allowed = new List<ModTypes>
+        {
+            ModTypes.Skins, ModTypes.Sound, ModTypes.Misc
+        };
+        public List<ModTypes> CanBeAllowed = new List<ModTypes>
+        {
+            ModTypes.Icons,ModTypes.Zoom
+        };
+    }
+}
